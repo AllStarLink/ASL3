06a937568708fdc9e5f73c6f937561b5ca6f9796
Jason D. McCormick <jason@mfamily.org>
2025-08-10 15:18:42 -0400

    build deps

--- builder/dockerbuild.sh
+++ builder/dockerbuild.sh
@@ -82,7 +82,8 @@ ALL_PKG_ROOT=$(dirname ${PDIR})
 echo "ALL_PKG_ROOT: ${ALL_PKG_ROOT}"
 echo "GH_REL: ${GH_REL}"
 
-D_TAG="asl3_builder.${OPERATING_SYSTEMS}.${ARCH}${REPO_ENV}"
+
+D_TAG="$(echo "${GH_REPO_NAME}" | cut -d'/' -f2).${OPERATING_SYSTEMS}.${ARCH}${REPO_ENV}"
 
 docker build -f $DIR/Dockerfile -t $D_TAG \
 	--build-arg ARCH="$ARCH" \
@@ -94,7 +95,7 @@ docker build -f $DIR/Dockerfile -t $D_TAG \
 docker run -v $ALL_PKG_ROOT:/build $D_TAG
 
 DEBIAN_FRONTEND=noninteractive apt-get -y install gh
-gh release upload -R AllStarLink/asl3 $GH_REL $ALL_PKG_ROOT/_debs/*.deb
+gh release upload -R ${GH_REPO_NAME} $GH_REL $ALL_PKG_ROOT/_debs/*.deb
 
 docker image rm --force $D_TAG
 
