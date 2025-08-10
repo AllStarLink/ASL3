4256aff182c57ffb28fb5cad6a1be4cedfaf9853
Jason D. McCormick <jason@mfamily.org>
2025-08-10 14:43:39 -0400

    +trixie

--- builder/dockerbuild.sh
+++ builder/dockerbuild.sh
@@ -21,7 +21,7 @@ while [[ $# -gt 0 ]]; do
       shift
       ;;
 	-r)
-      APTLY_REPO="asl3-$2"
+      REPO_LEVEL="$2"
       shift
       shift	
       ;;
@@ -65,10 +65,13 @@ else
   REPO_ENV=""
 fi
 
+APTLY_REPO="asl3-${OPERATING_SYSTEMS}-${REPO_LEVEL}"
+
 ## Need to clean this up to be more elegant
 echo "Architectures: $ARCH"
 echo "Targets: $TARGETS"
 echo "Operating Systems: $OPERATING_SYSTEMS"
+echo "Aptly Repo: ${APTLY_REPO}"
 echo "PWD: $(pwd)"
 echo "BS: ${BASH_SOURCE[0]}"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
@@ -79,7 +82,7 @@ ALL_PKG_ROOT=$(dirname ${PDIR})
 echo "ALL_PKG_ROOT: ${ALL_PKG_ROOT}"
 echo "GH_REL: ${GH_REL}"
 
-D_TAG="asl3_builder.${OPERATING_SYSTEMS}.${ARCH}${REPO_ENV}"
+D_TAG="asl-apt-repos_builder.${OPERATING_SYSTEMS}.${ARCH}${REPO_ENV}"
 
 docker build -f $DIR/Dockerfile -t $D_TAG \
 	--build-arg ARCH="$ARCH" \
@@ -91,7 +94,7 @@ docker build -f $DIR/Dockerfile -t $D_TAG \
 docker run -v $ALL_PKG_ROOT:/build $D_TAG
 
 DEBIAN_FRONTEND=noninteractive apt-get -y install gh
-gh release upload -R AllStarLink/asl3 $GH_REL $ALL_PKG_ROOT/_debs/*.deb
+gh release upload -R AllStarLink/asl-apt-repos $GH_REL $ALL_PKG_ROOT/_debs/*.deb
 
 docker image rm --force $D_TAG
 
